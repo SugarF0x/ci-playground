8b94d95a17134edac98e6d4a752f26c753bc4201
Alexey Tuzov <sugarfox.dev@gmail.com>
2022-10-30 14:35:03 +0300

    feat: cock and ball

--- pages/index.vue
+++ pages/index.vue
@@ -8,6 +8,7 @@ console.log('dummy ass shit')
 console.log('dummy ass shit')
 console.log('i am with type checks')
 console.log('cock')
+console.log('add more cock and additional ball')
 </script>
 
 <template>
